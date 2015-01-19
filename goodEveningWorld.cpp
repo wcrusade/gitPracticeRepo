5de7b074b5392e73ad7a981841e07be32357c80d
wcrusade <wcrusade@yahoo.com>
2015-01-18 22:19:36 -0800

    Add goodByeWorld.cpp. Add new comments goodEveningWorld.cpp

--- goodEveningWorld.cpp
+++ goodEveningWorld.cpp
@@ -3,6 +3,7 @@ using namespace std;
 //made in github. not local
 //good evening world
 
+//NEW EDIT: now editing on local machine. NOT github
 int main()
 {
   cout << "Good Evening World" << endl;
---
+++ goodbyeWorldTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//goodbye world
+int main()
+{
+	cout << "Goodbye World" << endl;
+	
+
+	return 0;
+}
