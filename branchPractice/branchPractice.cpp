cfb3cc3028e579f312bd22b49fa16969b3bdfb97
wcrusade <wcrusade@yahoo.com>
2015-01-18 23:38:48 -0800

    add branchPractice Folder

---
+++ branchPractice/branchPractice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//made on local machine
+//this is going to go in a new folder
+//that is being put into a branch
+
+int main()
+{
+	cout << "This will be pushed into a new branch with the choclate.jpeg";
+
+
+}
