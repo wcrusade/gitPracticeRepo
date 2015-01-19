8c16b86a9896b2591c844010769de1b41981fa38
wcrusade <wcrusade@yahoo.com>
2015-01-19 00:22:13 -0800

    trying to add branchPractice folder into new branch 'cats' along with a new file 'cats.cpp'

---
+++ branchPractice/cats.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//made on local machine
+//this cat file
+//will be put into the branchPractice folder
+//but now the entire folder along with
+//this additional file will be put into a separate branch from master
+
+int main()
+{
+	cout << "Meow cat";
+
+
+}
