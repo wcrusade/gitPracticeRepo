e17368d58bc386c7b5b3c9f27de36bf0b697b2d3
wcrusade <wcrusade@yahoo.com>
2015-01-18 22:41:58 -0800

    Add in goodMondayWorld.cpp

---
+++ goodMondayWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//made on local machine
+//made after good sunday world was made on github
+
+
+int main()
+{
+  cout << "Good Monday World" << endl;
+  return 0;
+}
